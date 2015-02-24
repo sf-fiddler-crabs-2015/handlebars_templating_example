34577d3104f13b7d4cb6adecbee40930f6fc047d
Jennifer Gilbert <jennifer@devbootcamp.com>
2015-02-23 16:02:45 -0800

    Get squirrels via AJAX and drop them onto the page for a nonideal user experience

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 /*
 OUR MISSION: We need to get some squirrels from somewheres, and we need to put those squirrels on the page!
 */
+
+// Retrieve some squirrels from Reddit
+$.ajax({
+  url: "http://www.reddit.com/r/squirrels.json"
+}).done(function(data) {
+  console.log(data);
+  $('#results').html(JSON.stringify(data));
+});
