a17b5184ca7a7b027deaf59e37061430b484714a
Jennifer Gilbert <jennifer@devbootcamp.com>
2015-02-23 16:25:42 -0800

    Roll back to fresh start (completed code is on 'finished' branch)

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
 /*
 OUR MISSION: We need to get some squirrels from somewheres, and we need to put those squirrels on the page!
 */
-
-// Retrieve some squirrels from Reddit
-function getSquirrels() {
-  // Retrieve Handlebars template from the HTML
-  var source = $('#squirrels-template').html();
-  var template = Handlebars.compile(source);
-  var context = {squirrels: []};
-
-  $.ajax({
-    url: "http://www.reddit.com/r/squirrels.json"
-  }).done(function(data) {
-    context.squirrels = data.data.children;
-    $('#results').html(template(context));
-  });
-}
-
-$(document).ready(function() {
-  getSquirrels();
-});
-
