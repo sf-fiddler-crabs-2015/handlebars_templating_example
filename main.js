9031c7123fa828b228251612ee9c1f2313cb060e
Jennifer Gilbert <jennifer@devbootcamp.com>
2015-02-25 09:34:52 -0800

    Add commented steps because I keep forgetting how to live-code this thing

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 /*
 OUR MISSION: We need to get some squirrels from somewheres, and we need to put those squirrels on the page!
 */
+
+// write a function that gets some squirrels with an AJAX call
+// when the AJAX call returns, take the squirrels to Uglytown by plopping them into your HTML
+// run the getSquirrels function call when the document is ready
+
+// use Handlebars instead to display the squirrels in an awesome way!
